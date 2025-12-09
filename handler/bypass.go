b9c78eda48dbf52870151e271c987cd33bfd7c0a
Kirill Mironov <22131753+KirillMironov@users.noreply.github.com>
2025-12-09 20:10:43 +0300

    bypass: new client hello splitting

--- handler/bypass.go
+++ handler/bypass.go
@@ -59,6 +59,10 @@ func (b *Bypass) Handle(ctx context.Context, conn net.Conn, sni string, reader i
 	}
 	defer targetConn.Close()
 
+	if tcp, ok := targetConn.(*net.TCPConn); ok {
+		_ = tcp.SetNoDelay(true)
+	}
+
 	clientHelloBuf := make([]byte, b.config.ClientHello.BufferSize)
 	n, err := reader.Read(clientHelloBuf)
 	if err != nil {
@@ -66,26 +70,64 @@ func (b *Bypass) Handle(ctx context.Context, conn net.Conn, sni string, reader i
 	}
 	clientHelloData := clientHelloBuf[:n]
 
-	// write ClientHello packets in chunks
-	for i := 0; i < len(clientHelloData); i += int(b.config.ClientHello.ChunkSize) {
-		end := i + int(b.config.ClientHello.ChunkSize)
-		if end > len(clientHelloData) {
-			end = len(clientHelloData)
-		}
-
-		chunk := clientHelloData[i:end]
-
-		_, err = targetConn.Write(chunk)
-		if err != nil {
-			slog.ErrorContext(ctx, "failed to write chunk", slog.Any("error", err))
-			return
-		}
-
-		time.Sleep(b.config.ClientHello.ChunkDelay)
+	if err = b.splitClientHello(clientHelloData, targetConn); err != nil {
+		slog.ErrorContext(ctx, "failed to split ClientHello", slog.Any("error", err))
+		return
 	}
 
 	var wg sync.WaitGroup
 	wg.Go(func() { _, _ = io.Copy(conn, targetConn) })
 	wg.Go(func() { _, _ = io.Copy(targetConn, reader) })
 	wg.Wait()
+}
+
+func (b *Bypass) splitClientHello(clientHelloData []byte, targetConn net.Conn) error {
+	isTLS := len(clientHelloData) > 5 && clientHelloData[0] == 0x16
+	chunkSize := int(b.config.ClientHello.ChunkSize)
+
+	// split only if we have enough data
+	if isTLS && chunkSize > 0 && len(clientHelloData) > 5+chunkSize {
+		// parse the total length of the TLS payload from the header
+		totalLen := int(clientHelloData[3])<<8 | int(clientHelloData[4])
+
+		// ensure we actually read the full record
+		if len(clientHelloData) >= 5+totalLen {
+			// the first chunk
+			// [16] [VerMajor] [VerMinor] [LenHigh] [LenLow]
+			header1 := []byte{
+				0x16,
+				clientHelloData[1],
+				clientHelloData[2],
+				byte(chunkSize >> 8),
+				byte(chunkSize),
+			}
+			if _, err := targetConn.Write(header1); err != nil {
+				return err
+			}
+			if _, err := targetConn.Write(clientHelloData[5 : 5+chunkSize]); err != nil {
+				return err
+			}
+
+			// the rest
+			remaining := totalLen - chunkSize
+			header2 := []byte{
+				0x16,
+				clientHelloData[1],
+				clientHelloData[2],
+				byte(remaining >> 8),
+				byte(remaining),
+			}
+			if _, err := targetConn.Write(header2); err != nil {
+				return err
+			}
+			if _, err := targetConn.Write(clientHelloData[5+chunkSize : 5+totalLen]); err != nil {
+				return err
+			}
+		} else {
+			_, err := targetConn.Write(clientHelloData)
+			return err
+		}
+	}
+
+	return nil
 }
